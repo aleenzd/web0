cda3993d262299fdeb8713ae18cceff7cca47c24
Aleen Zeidan <aleenzeidan2@gmail.com>
2021-10-17 23:15:22 +0300

    zubaida

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express=require('express')
-const app=express()
-var cors=require('cors');
+// const express=require('express')
+// const app=express()
+// var cors=require('cors');
 
-app.use(cors());
+// app.use(cors());
 
-app.get('/',function(req,res){
-    res.send("<h1>Hello world</h1>");
-})
-app.listen(process.env.PORT || 3000,
-    () => console.log("server is running . . ."));
+// app.get('/',function(req,res){
+//     res.send("<h1>Hello world</h1>");
+// })
+// app.listen(process.env.PORT || 3000,
+//     () => console.log("server is running . . ."));
+function test()
+{fetch('https://secret-hollows-21282.herokuapp.com/')
+.then(response => response.json())
+.then(json => console.log(json))
+}
